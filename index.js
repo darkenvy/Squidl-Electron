ad8b4d17cdcf0b402b5818fcbcb5a56487c769f1
darkenvy <darkenvy6@gmail.com>
2016-09-10 20:47:51 -0700

    before foundation

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
 const electron = require('electron');
 let { app, BrowserWindow } = electron;
+const fs = require('fs');
+const path = require('path');
 app.on('ready', function () {
     let mainWindow = new BrowserWindow({
         width: 800,
         height: 600
     });
-    mainWindow.loadURL('file://' + __dirname + "/index.html");
+    mainWindow.loadURL('file://' + __dirname + "/html/index.html");
+    let p = path.join(__dirname, '..');
+    // fs.readdir(p, 'utf8', function(err, data) {
+    //   if (err) return console.log(err);
+    //   console.log(data);
+    // })
 });
--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 'use strict';
 
-const electron = require('electron')
-let {app, BrowserWindow} = electron
+const electron = require('electron');
+let {app, BrowserWindow} = electron;
+const fs = require('fs');
+const path = require('path');
 
 app.on('ready', function() {
   let mainWindow = new BrowserWindow({
     width: 800,
     height: 600
   })
-  mainWindow.loadURL('file://' + __dirname + "/index.html")
+  mainWindow.loadURL('file://' + __dirname + "/index.html");
+  let p = path.join(__dirname, '..');
+  // fs.readdir(p, 'utf8', function(err, data) {
+  //   if (err) return console.log(err);
+  //   console.log(data);
+  // })
 })
